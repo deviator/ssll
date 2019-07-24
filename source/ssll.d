37f3e961edf899d75f8337f03285922838cc960e
deviator <code.viator@gmail.com>
2019-07-24 14:39:49 +0300

    add windows support

--- source/ssll.d
+++ source/ssll.d
@@ -1,27 +1,81 @@
 ///
 module ssll;
 
-import core.sys.posix.dlfcn;
-
 import std.string : toStringz;
 import std.exception : enforce;
+
+version (Posix)
+{
+    import core.sys.posix.dlfcn;
+}
+else version (Windows)
+{
+    import core.sys.windows.winbase;
+    import core.sys.windows.windef : HINSTANCE;
+}
+else static assert(0, "unknown platform");
 
 struct ApiUDA { string libname; }
 auto api(string lname="lib") { return ApiUDA(lname); }
 
 string apiFunctionPointerName(string f) { return "__"~f~"_dlg"; }
 
+version (Posix) alias LibHandler = void*;
+version (Windows) alias LibHandler = HINSTANCE;
+
 ///
-void* loadLibrary(string name)
+LibHandler loadLibrary(string name)
 {
-    return dlopen(name.toStringz, RTLD_LAZY);
+    if (name.length == 0) return null;
+
+    version (Posix)
+        return dlopen(name.toStringz, RTLD_LAZY);
+    version (Windows)
+    {
+        import core.stdc.stdlib : free, malloc;
+
+        import core.sys.windows.winnls : CP_UTF8, MultiByteToWideChar;
+        import core.sys.windows.winnt : WCHAR;
+
+        auto len = MultiByteToWideChar(CP_UTF8, 0, name.ptr,
+                                cast(int)name.length, null, 0);
+
+        if (len == 0) return null;
+
+        auto buf = cast(WCHAR*)malloc((len+1) * WCHAR.sizeof);
+        if (buf is null) return null;
+        scope (exit) free(buf);
+
+        len = MultiByteToWideChar(CP_UTF8, 0, name.ptr,
+                                cast(int)name.length, buf, len);
+        if (len == 0) return null;
+
+        buf[len] = '\0';
+        return LoadLibraryW(buf);
+    }
 }
 
 ///
-void unloadLibrary(ref void* lib)
+void unloadLibrary(ref LibHandler lib)
 {
-    dlclose(&lib);
-    lib = null;
+    version (Posix)
+    {
+        dlclose(&lib);
+        lib = null;
+    }
+    version (Windows)
+    {
+        FreeLibrary(lib);
+        lib = null;
+    }
+}
+
+auto getSymbol(LibHandler lib, string name)
+{
+    version (Posix)
+        return dlsym(lib, name.toStringz);
+    version (Windows)
+        return GetProcAddress(lib, name.toStringz);
 }
 
 private enum __initDeclare = q{
@@ -29,7 +83,7 @@ private enum __initDeclare = q{
     import std.typecons;
     import std.traits;
     import std.string;
-    import core.sys.posix.dlfcn : dlsym;
+    //import core.sys.posix.dlfcn : dlsym;
 
     enum __dimmy;
     alias __this = AliasSeq!(__traits(parent, __dimmy))[0];
@@ -53,6 +107,13 @@ string rtLib()
     return __initDeclare ~ q{
     mixin("auto __fnc = %s;".format(apiFunctionPointerName(__name)));
     } ~ __callDeclare;
+}
+
+enum LoadApiSymbolsVerbose
+{
+    none,
+    message,
+    assertion
 }
 
 mixin template apiSymbols()
@@ -85,16 +146,27 @@ mixin template apiSymbols()
 
     alias apiFuncs = funcsByUDA!(__traits(parent, __dimmy), ApiUDA);
 
-    void loadApiSymbols()
+    void loadApiSymbols(LoadApiSymbolsVerbose verbose=LoadApiSymbolsVerbose.none)
     {
-        import std.string;
-        import core.sys.posix.dlfcn;
+        import std.format : format;
+        import std.stdio : stderr;
+
         foreach (f; apiFuncs)
         {
             enum libname = getUDAs!(f, ApiUDA)[$-1].libname;
             enum fname = __traits(identifier, f);
             enum pname = apiFunctionPointerName(fname);
-            mixin(format(`%2$s = cast(typeof(%2$s))dlsym(%3$s, "%1$s".toStringz);`, fname, pname, libname));
+            mixin(format!`%2$s = cast(typeof(%2$s))getSymbol(%3$s, "%1$s");`(fname, pname, libname));
+            if (mixin(pname ~ " is null"))
+            {
+                auto errmsg = format!`can't find '%s' function`(fname);
+                with (LoadApiSymbolsVerbose) final switch (verbose) 
+                {
+                    case none: break;
+                    case message: stderr.writeln(errmsg); break;
+                    case assertion: assert(0, errmsg);
+                }
+            }
         }
     }
 
@@ -108,7 +180,7 @@ mixin template funcPointers(funcs...)
     else static if (funcs.length == 1)
     {
         alias __this = funcs[0];
-        mixin(`private extern(C) @nogc nothrow ReturnType!__this function(Parameters!__this) %s;`
+        mixin(`private __gshared extern(C) @nogc nothrow ReturnType!__this function(Parameters!__this) %s;`
                 .format(apiFunctionPointerName(__traits(identifier, __this))));
     }
     else
